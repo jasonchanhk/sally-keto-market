156798734f86557ae65640923cfa7fff10c6fbea
Sally <sallydaveytv@gmail.comgit config --global user.name Sdev77>
2022-06-24 14:27:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const fs = require(`fs`); // calling this file system will give us access to reading data and writing data. It will return a object with a lot of functions that we can use.
+
+const http = require("http"); // This module gives us networking capabilities such as building an http server.
+
+const url = require("url");
+
+/////////////////////////////////////////
+//FILES
+///////////////////////////////////////////
+// // How to read from files in a synchronous way.
+// const textIn = fs.readFileSync(`./txt/input.txt`, `utf-8`); // calling this function will read the data and return it to us.
+// // console.log(textIn);
+
+// // //How to Write to files in a synchronous way.
+// // const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+
+// // fs.writeFileSync(`./txt/output.txt`, textOut);
+// // // console.log(`file written`);
+
+// //NON- Blocking Asynchronous way to read files.
+
+// fs.readFile(`./txt/start.txt`, "utf-8", (err, data1) => {
+//   if (err) return console.log(`ERROR 💥`);
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log(`your file has been written!`);
+//       });
+//     });
+//   });
+// });
+
+// console.log(`will read file`);
+///////////////////////////////////////////
+//SERVER
+//////////////////////////////////////////
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+
+    res.end(output);
+
+    //Product page
+  } else if (pathname === "/products") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+
+    //API
+  } else if (pathname === "/api") {
+    // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+    //   const productData = JSON.parse(data);
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    // });
+
+    //Not found
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
+
+/////////////////////////////////////////
+//
+////////////////////////////////////////
